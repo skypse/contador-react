7f19712fda11e19c494f4dca2d5449085f79d86c
skypse <skypse0136@gmail.com>
2024-05-12 10:23:35 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Contador from './components/Contador.jsx'
+import './index.css'
+import './App.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Contador />
+  </React.StrictMode>,
+)
